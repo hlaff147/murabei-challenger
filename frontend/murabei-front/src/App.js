ca48e330db08a9a24fa41e49dd95eab5562e03c1
hlaff147 <hlaff@cin.ufpe.br>
2023-07-18 20:46:11 -0300

    feat: initialize react js project

---
+++ frontend/murabei-front/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <NavBar />
+      {/* O resto do seu app vai aqui */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/murabei-front/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/sobre">Sobre</a></li>
+                <li><a href="/contato">Contato</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;

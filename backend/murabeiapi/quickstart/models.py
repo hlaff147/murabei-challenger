7e2a92c8be7ef0f08cb79ec62ba6c7e06d35807b
hlaff147 <hlaff@cin.ufpe.br>
2023-07-23 06:01:47 -0300

    generate migratioons and update models

--- backend/murabeiapi/quickstart/models.py
+++ backend/murabeiapi/quickstart/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 # Create your models here.
@@ -11,7 +12,7 @@ class Author(models.Model):
 
 class Subjects(models.Model):
     id = models.AutoField(primary_key=True)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, unique=True)
 
 
 class Book(models.Model):
@@ -19,3 +20,60 @@ class Book(models.Model):
     name = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     subjects = models.ManyToManyField(Subjects)
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, cpf, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not cpf:
+            raise ValueError("Users must have a CPF")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            cpf=cpf,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, cpf, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            cpf=cpf,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    cpf = models.CharField(max_length=14, unique=True)
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['cpf']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin

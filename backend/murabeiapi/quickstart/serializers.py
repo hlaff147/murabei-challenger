bd355973e3c0604fb11a792d7963d3faf5816151
hlaff147 <hlaff@cin.ufpe.br>
2023-07-29 07:38:59 -0300

    feat: add login and user views/serializers

--- backend/murabeiapi/quickstart/serializers.py
+++ backend/murabeiapi/quickstart/serializers.py
@@ -1,15 +1,59 @@
 # serializers.py
 from rest_framework import serializers
-from .models import Book, Author
+from .models import Book, Author, Subjects, User
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'name', 'author', 'subjects']
+        extra_kwargs = {
+            'name': {'required': True},
+            'author': {'required': True},
+            'subjects': {'required': True},
+        }
+
+    def to_representation(self, instance):
+        self.fields['author'] = AuthorSerializer(read_only=True)
+        self.fields['subjects'] = SubjectsSerializer(many=True, read_only=True)
+        return super(BookSerializer, self).to_representation(instance)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['id', 'name', 'email', 'age']
+        extra_kwargs = {
+            'name': {'required': True},
+            'email': {'required': True},
+            'age': {'required': True},
+        }
+
+
+class SubjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subjects
+        fields = ['id', 'description']
+        extra_kwargs = {
+            'description': {'required': True},
+        }
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'name', 'email', 'cpf', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
--- backend/murabeiapi/quickstart/views.py
+++ backend/murabeiapi/quickstart/views.py
@@ -1,7 +1,16 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Book, Author
-from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.permissions import IsAdminUser
+
+from .models import Book, Author, Subjects, User
+from .serializers import BookSerializer, AuthorSerializer, SubjectsSerializer, UserSerializer, LoginSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import status
+from rest_framework import mixins
+from django.contrib.auth.hashers import check_password
+
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -12,3 +21,51 @@ class BookViewSet(viewsets.ModelViewSet):
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+
+class SubjectsViewSet(viewsets.ModelViewSet):
+    queryset = Subjects.objects.all()
+    serializer_class = SubjectsSerializer
+
+
+class UserViewSet(mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = (AllowAny,)
+        else:
+            self.permission_classes = (IsAdminUser,)
+
+        return super(UserViewSet, self).get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        try:
+            email = request.data["email"]
+            cpf = request.data["cpf"]
+            password = request.data["password"]
+            User.objects.create_user(email=email, cpf=cpf, password=password)
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"message": "Could not create user due to {}".format(e)},
+                            status=status.HTTP_400_BAD_REQUEST)
+class LoginViewSet(viewsets.ViewSet):
+    serializer_class = LoginSerializer
+
+    def create(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            user = User.objects.get(email=serializer.validated_data['email'])
+            if user and check_password(serializer.validated_data['password'], user.password):
+                return Response({'message': 'Successful login.'}, status=status.HTTP_200_OK)
+            else:
+                raise User.DoesNotExist
+        except User.DoesNotExist:
+            return Response({'message': 'Invalid login credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
